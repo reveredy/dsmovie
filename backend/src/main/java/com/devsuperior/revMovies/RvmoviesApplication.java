8716ffd8b39ba5a1483a680730b010ae08286dc0
reveredy <carlos.andre.albanus@hotmail.com>
2022-03-07 21:09:52 -0300

    created project

---
+++ backend/src/main/java/com/devsuperior/revMovies/RvmoviesApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.revMovies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RvmoviesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RvmoviesApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devsuperior/revMovies/RvmoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.revMovies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RvmoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
